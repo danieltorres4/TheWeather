3aa668d19b248fdeed3cbdf34a6970d3ed4bec5d
danieltorres4 <dst990502@outlook.com>
2023-03-27 11:48:09 -0600

    Initial Commit

---
+++ TheWeather/TheWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheWeatherApp.swift
+//  TheWeather
+//
+//  Created by Iván Sánchez Torres on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TheWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
